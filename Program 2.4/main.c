e079d9d4b1a7a69e0dbde64ac885e0b14fc9e0be
jeancarlos2828 <jeanjomar04@gmail.com>
2025-03-06 14:27:02 -0400

    Primer Commit

---
+++ Program 2.4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Incremento precio.
+El programa, al recibir como dato el precio de un producto, incrementa al
+mismo 11% si es menor a 1500$ y un 8% en caso contrario (mayor o igual).
+
+PRE Y NPR: variables de tipo real. */
+
+void main(void)
+{
+    float PRE, NPR;
+    printf("ingrese el precio del producto: ");
+    scanf("%f", &PRE);
+    if (PRE < 1500)
+        NPR= PRE * 1.11;
+    else
+        NPR = PRE * 1.08;
+    printf("\nNuevo precio del producto: %8.2f", NPR);
+}
