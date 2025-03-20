c0513712e1bd5062a2ebe40d0480c9352f35007e
jeancarlos2828 <146147140+jeancarlos2828@users.noreply.github.com>
2025-03-20 11:14:47 -0400

    Add files via upload

---
+++ Program 4.16/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Temperaturas.
+El programa recibe como datos 24 números reales que representan las
+temperaturas en el exterior en un período de 24 horas. Calcula el
+promedio del día y las temperaturas máxima y mínima con la hora en la
+que se registraron. */
+
+void Acutem(float);
+void Maxima(float, int);  /* Prototipos de funciones. */
+void Minima(float, int);
+
+float ACT = 0.0;
+float MAX = -50.0;      /* Variables globales. */
+float MIN = 60.0;
+
+int HMAX;
+int HMIN;
+
+/* Variables globales. ACT se utiliza para acumular las temperaturas,
+por esa razón se inicializa en cero. MAX se utiliza para calcular la
+máxima; se inicializa en –50 para que el primer valor que se ingrese
+modifique su contenido. MIN se usa para calcular la mínima; se
+inicializa con un valor muy alto para que el primer valor ingresado
+modifique su contenido. HMAX y HMIN se utilizan para almacenar el
+horario en que se produjeron las temperaturas máxima y mínima,
+respectivamente. */
+
+void main(void)
+{
+    float TEM;
+    int I;
+    for (I = 1; I <= 24; I++)
+{
+    printf("Ingresa la temperatura de la hora %d: ", I);
+    scanf("%f", &TEM);
+    Acutem(TEM);
+    Maxima(TEM, I); /* Llamada a las funciones. Paso de parámetros
+    por valor. */
+    Minima(TEM, I);
+}
+}
